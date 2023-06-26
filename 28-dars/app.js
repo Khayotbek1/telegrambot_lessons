2f51d67a35565b960eed7e6ee114bb0ea8ebd62a
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-06-26 09:40:22 +0500

    Setter Getter

--- 28-dars/app.js
+++ 28-dars/app.js
@@ -18,7 +18,38 @@ class Student {
 }
 
 const hayot = new Student('Hayot', 'student')
-
 hayot.editDegree = 'Master'
-
 console.log(hayot.degree);
+
+
+
+
+
+class Cars {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    get info() {
+        return [this.name, this.color];
+    }
+
+    set editColor(color) {
+        this.color = color;
+        return color;
+    }
+
+    set editName(name) {
+        this.name = name;
+        return name;
+    }
+}
+
+const car = new Cars('bmw', 'black');
+
+car.editName = 'audi';
+car.editColor = 'white';
+console.log(car);
+
+
