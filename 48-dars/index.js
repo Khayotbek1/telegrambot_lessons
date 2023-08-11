8033ef1a448bec5464524c279a68f77f9fb595cb
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-08-12 00:16:35 +0500

    telegramBotApi

--- 48-dars/index.js
+++ 48-dars/index.js
@@ -1,11 +1,14 @@
 import TelegramBot from "node-telegram-bot-api";
+
 const bot = new TelegramBot("6528920785:AAEUDQiju_-g4EnF8mdQIb2vhne0Jvv1yyk", {
   polling: true,
 });
 
 bot.on("message", (update) => {
-  if ((update, text == "./start")) {
-    bot.sendMessage(update.chat.id, "Enter your name");
+  if (update.text == "/start") {
+    bot.sendMessage(update.chat.id, "<b>Enter your name</b>", {
+      parse_mode: "HTML",
+    });
   } else {
     data.push(update.text);
   }
